9bac55e8708acd2634b1a262af52011b61e2a94f
Elrojizo17 <alejandroescobard85@gmail.com>
2025-11-26 17:54:06 -0500

    Corregidas 2.0

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,12 @@ import Header from "./components/Header";
 import Section from "./components/Section";
 import Card from "./components/Card";
 
-import avatar from "./assets/davinson.png";
-import univalle from "./assets/univalle.png";
-import bolivariano from "./assets/bolivariano.png";
-import logoRedis from "./assets/logo-redis.png";
-import whatsapp from "./assets/whatsapp.png";
-import gmail from "./assets/gmail.png";
-import linkedin from "./assets/linkedin.png";
-import github from "./assets/github.png";
-
 function App() {
   return (
     <>
-      <Header avatar={avatar} />
+      <Header avatar="/images/Davinson.png" />
 
       <main>
-
         {/* Perfil */}
         <Section id="Perfil" title="Perfil">
           <Card
@@ -37,42 +27,40 @@ function App() {
         {/* Herramientas */}
         <Section id="Herramientas" title="Herramientas">
           <div className="grid grid--cards">
-
             <Card
-              image="https://quantumzeitgeist.com/wp-content/uploads/pythoned.png"
+              image="/images/pythoned.png"
               title="Python"
               description="Lenguaje versátil y potente."
               badge="Desarrollo básico en Python 3"
             />
 
             <Card
-              image="https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg"
+              image="/images/pythoned.png"
               title="HTML 5"
               description="Estructuración semántica y moderna."
               badge="Diseño responsivo"
             />
 
             <Card
-              image="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"
+              image="/images/pythoned.png"
               title="CSS"
               description="Estilos modernos y adaptables."
               badge="UI Moderna"
             />
 
             <Card
-              image="https://cdn-1.webcatalog.io/catalog/canva-cn/canva-cn-icon.png"
+              image="/images/pythoned.png"
               title="Canva"
               description="Diseño gráfico intuitivo."
               badge="Contenido visual"
             />
 
             <Card
-              image="https://upload.wikimedia.org/wikipedia/commons/2/29/Postgresql_elephant.svg"
+              image="/images/pythoned.svg"
               title="PostgreSQL"
               description="Gestión de bases de datos."
               badge="Diseño y administración"
             />
-
           </div>
         </Section>
 
@@ -80,21 +68,21 @@ function App() {
         <Section id="Experiencia" title="Experiencia">
           <div className="grid grid--cards">
             <Card
-              image={univalle}
+              image="/images/univalle.png"
               title="Monitor Administrativo"
               description="Apoyo en procesos administrativos y académicos."
               badge="Gestión"
             />
 
             <Card
-              image={logoRedis}
+              image="/images/redsi.png"
               title="RREDSI"
               description="Apoyo en logística y organización de eventos."
               badge="Logística"
             />
 
             <Card
-              image="https://cdn-icons-png.freepik.com/512/9353/9353735.png"
+              image="/images/pythoned.png"
               title="Eventos"
               description="Coordinación y apoyo en eventos institucionales."
               badge="Organización"
@@ -106,19 +94,19 @@ function App() {
         <Section id="Estudios" title="Estudios">
           <div className="grid grid--cards">
             <Card
-              image={bolivariano}
+              image="/images/bolivariano.png"
               title="Bachiller Técnico"
               description="Graduado en 2022."
             />
 
             <Card
-              image="https://cancelar.com.co/wp-content/uploads/2022/09/ASW-cancelar-1.png"
+              image="/images/pyhtoned.png"
               title="Curso de Inglés"
               description="Certificado nivel B2."
             />
 
             <Card
-              image={univalle}
+              image="/images/univalle.png"
               title="Desarrollo de Software"
               description="Actualmente en 6to semestre."
             />
@@ -129,30 +117,28 @@ function App() {
         <Section id="Contacto" title="Contacto">
           <Card>
             <div className="perfil__images">
-
-              <a href="https://wa.me/573146750350" target="_blank">
-                <img src={whatsapp} alt="Whatsapp" />
+              <a href="https://wa.me/573146750350" target="_blank" rel="noreferrer">
+                <img src="/images/whatsapp.png" alt="Whatsapp" />
               </a>
 
               <a href="mailto:alejandroescobard85@gmail.com">
-                <img src={gmail} alt="Correo" />
+                <img src="/images/gmail.svg" alt="Correo" />
               </a>
 
               <a
                 href="https://www.linkedin.com/in/alejandro-escobar-15159b270"
                 target="_blank"
+                rel="noreferrer"
               >
-                <img src={linkedin} alt="LinkedIn" />
+                <img src="/images/linkedin.png" alt="LinkedIn" />
               </a>
 
-              <a href="https://github.com/Elrojizo17" target="_blank">
-                <img src={github} alt="GitHub" />
+              <a href="https://github.com/Elrojizo17" target="_blank" rel="noreferrer">
+                <img src="/images/github.png" alt="GitHub" />
               </a>
-
             </div>
           </Card>
         </Section>
-
       </main>
 
       <footer className="site-footer">
